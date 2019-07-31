e89fd3620b820b20637153af83a1e6935b165200
Seungje Jun <ninanung@naver.com>
2019-07-31 14:33:49 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useMemo, useCallback, useRef} from 'react';
+import React, {useEffect, useState, useMemo, useCallback, useRef, useReducer, useContext} from 'react';
 import './App.css';
 
 const UseStateExample = () => {
@@ -12,10 +12,40 @@ const UseStateExample = () => {
   )
 }
 
+const UseEffectExample = () => {
+  const [test, setTest] = useState('initial value');
+  const [test2, setTest2] = useState('initial value');
+  useEffect(() => {
+    console.log('state가 변경될 때 마다 호출!');
+  })
+
+  useEffect(() => {
+    console.log('test state에 대해서만 호출!')
+  }, [test])
+
+  useEffect(() => {
+    console.log('test2 state에 대해서만 호출!')
+  }, [test2])
+
+  useEffect(() => {
+    console.log('첫 렌더링에만 호출')
+  }, [])
+
+  return (
+    <div>
+      <p>{test}</p>
+      <p>{test2}</p>
+      <input onChange={(e) => {setTest(e.target.value)}} />
+      <input onChange={(e) => {setTest2(e.target.value)}} />
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <UseStateExample />
+      <UseEffectExample />
     </div>
   );
 }
