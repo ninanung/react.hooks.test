f35eecaa0975beb68d6af1911aa11b727f45c88c
Seungje Jun <ninanung@naver.com>
2019-09-25 18:52:25 +0900

    create-react-app to my own webpack setting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import '../public/favicon.ico'
+import '../public/favicon.ico';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
